2c79254a7511a651d4cc035f21b4cc4221058faa
Fernando Torres <fertorresf@gmail.com>
2014-06-13 10:48:10 -0400

    Se añadio control del puenteH usado registros

--- Software/buttonMotor/buttonMotor.ino
+++ Software/buttonMotor/buttonMotor.ino
@@ -5,8 +5,8 @@
  2 pulsadores controlan la direccion de un motor que gira por
  un tiempo determinado.
  */
- 
- 
+
+
 int pinDer = 2;
 int pinIzq = 3;
 
@@ -16,41 +16,33 @@ int Ados = 6;
 
 int timeDelay = 2000;
 
-void setup(){
-  Serial.begin( 9600);
-  pinMode(   EN, OUTPUT);
-  pinMode( Auno, OUTPUT);
-  pinMode( Ados, OUTPUT);
-  
-  pinMode( pinDer, INPUT);
-  pinMode( pinIzq, INPUT);
-}
+// Variable "registerFlag" que determina si el puente H sera 
+// activado con digitalWrite() o on registros
+int registerFlag = 1; // 0 -> digitalWrite | 1 -> registros
 
-void loop(){
-
-  if ( digitalRead( pinDer) == HIGH){
-    moverDer();
+/* Funciones auxiliares */
+void moverDer( int registerFlag){
+  if( registerFlag == 0){
+    digitalWrite( EN, HIGH);
+    digitalWrite( Auno, HIGH);
+    digitalWrite( Ados, LOW);
   }
-  else if ( digitalRead( pinIzq) == HIGH){
-    moverIzq();
+  else if( registerFlag == 1){
+    PORTD = PORTD | B00110000;
   }
-  else {
-    detener();
-  }
-}
-
-void moverDer(){
-  digitalWrite( EN, HIGH);
-  digitalWrite( Auno, HIGH);
-  digitalWrite( Ados, LOW);
   delay( timeDelay);
   detener();
 }
 
 void moverIzq(){
-  digitalWrite( EN, HIGH);
-  digitalWrite( Auno, LOW);
-  digitalWrite( Ados, HIGH);
+  if( registerFlag == 0){
+    digitalWrite( EN, HIGH);
+    digitalWrite( Auno, LOW);
+    digitalWrite( Ados, HIGH);
+  }
+  else if( registerFlag == 1){
+    PORTD = PORTD | B01010000;
+  }
   delay( timeDelay);
   detener();
 }
@@ -58,4 +50,32 @@ void moverIzq(){
 void detener(){
   digitalWrite( EN, LOW);
 }
+
+/* Funciones principales */
+void setup(){
+  Serial.begin( 9600);
+
+  pinMode(   EN, OUTPUT);
+  pinMode( Auno, OUTPUT);
+  pinMode( Ados, OUTPUT);
+
+  pinMode( pinDer, INPUT);
+  pinMode( pinIzq, INPUT);
+}
+
+void loop(){
+  if ( digitalRead( pinDer) == HIGH){
+    moverDer( registerFlag);
+  }
+  else if ( digitalRead( pinIzq) == HIGH){
+    moverIzq( registerFlag);
+  }
+  else {
+    detener();
+  }
+}
+
+
+
+
 
